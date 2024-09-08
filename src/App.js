c30304db8aec009dd89003c4765aeae6a1a718ac
Gregory Gerard <wako057@gmail.com>
2024-09-08 10:30:53 +0200

    feat: add error navigation page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import ProductsPage from "./pages/Products";
+import ProductsPage from "./pages/ProductsPage";
 import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   { 
     path: '/', 
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
-    ]
+    ],
   },
 ]);
 
--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
-import classes from './RootLayout.module.css';
 
 function RootLayout() {
     return <>
         <MainNavigation />
-        <main className={classes.content}>
+        <main>
             <Outlet />
         </main>
     </>
