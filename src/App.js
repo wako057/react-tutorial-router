f662ab51a40db2986a344b6313a503f7aba3be8a
Gregory Gerard <wako057@gmail.com>
2024-09-08 09:52:04 +0200

    feat: legacy route (not working for me)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={HomePage} />
+    <Route path="/products" element={ProductsPage} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+// const router = createBrowserRouter([
+//   { path: '/', element: <HomePage /> },
+//   { path: '/products', element: <ProductsPage /> },
+// ]);
 
 function App() {
   return <RouterProvider router={router} />;
