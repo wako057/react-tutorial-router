acd4a4f9bf7130a7168e16f895bfba61e1e5c59c
Gregory Gerard <wako057@gmail.com>
2024-09-08 09:45:05 +0200

    chore: skeleton - samples routes

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <h1>My HomePage</h1>
+}
+
+export default HomePage;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+    return <h1>The Product Page</h1>
+}
+
+export default ProductsPage;
